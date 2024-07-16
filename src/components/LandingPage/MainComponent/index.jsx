033f17603ad734b64940da07c8d34ce59c99ba64
Vedant Chaudhary <vedantchaudhary2002@gmail.com>
2024-07-16 15:09:03 +0530

    added framer motion to phone image and heading in landing page

--- src/components/LandingPage/MainComponent/index.jsx
+++ src/components/LandingPage/MainComponent/index.jsx
@@ -1,28 +1,63 @@
-import React from 'react'
-import "./styles.css"
-import Button from '../../Common/Button'
-import iphone from '../../../assets/iphone.png'
-import gradient from '../../../assets/gradient.png'
+import React from "react";
+import "./styles.css";
+import Button from "../../Common/Button";
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png";
+import { motion } from "framer-motion";
 
 function MainComponent() {
   return (
-    <div className='flex-info'>
-      <div className='left-component'>
-        <h1 className='track-crypto-heading'>Track Crypto</h1>
-        <h1 className='real-time-heading'>Real Time.</h1>
-        <p className='info-text'> Track crypto through a public api in real time. Visit the dashboard to
-        do so!</p>
-        <div className='btn-flex'>
-          <Button text={"Dashboard"}/>
-          <Button text={"Share"} outlined="true"/>
-        </div>
+    <div className="flex-info">
+      <div className="left-component">
+        <motion.h1
+          className="track-crypto-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className="real-time-heading"
+        >
+          Real Time.
+        </motion.h1>
+        <p className="info-text">
+          {" "}
+          Track crypto through a public api in real time. Visit the dashboard to
+          do so!
+        </p>
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.5 }}
+          className="btn-flex"
+        >
+          <Button text={"Dashboard"} />
+          <Button text={"Share"} outlined="true" />
+        </motion.div>
       </div>
-      <div className='phone-container'>
-        <img src={iphone} alt="" className='iphone'/>
-        <img src={gradient} alt="" className='gradient'/>
+      <div className="phone-container">
+        <motion.img
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 2,
+            repeat: Infinity,
+          }}
+          src={iphone}
+          alt=""
+          className="iphone"
+        />
+        <img src={gradient} alt="" className="gradient" />
       </div>
     </div>
-  )
+  );
 }
 
-export default MainComponent
+export default MainComponent;
