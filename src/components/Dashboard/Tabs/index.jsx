02facece2bb78733d686bbbedf7d2bfb50fbbcec
Vedant Chaudhary <vedantchaudhary2002@gmail.com>
2024-07-16 18:08:57 +0530

    tabscomponent inside dashboard page is completed

--- src/components/Dashboard/Tabs/index.jsx
+++ src/components/Dashboard/Tabs/index.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function TabsComponent() {
+export default function TabsComponent() {
+  const [value, setValue] = React.useState("grid");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const style = {
+    color: "var(--white)",
+    width: "50vw",
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    textTransform: "capitalize",
+  };
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+        color: "#fff",
+      },
+    },
+  });
+
   return (
-    <div>
-      
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+        <TabList onChange={handleChange} variant="fullWidth">
+          <Tab label="Grid" value="grid" sx={style} />
+          <Tab label="List" value="list" sx={style} />
+        </TabList>
+        <TabPanel value="grid">
+          {" "}
+          <div>mapping for grids</div>
+        </TabPanel>
+        <TabPanel value="list">
+          <div>mapping for lists</div>
+        </TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
 }
-
-export default TabsComponent
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
-  </React.StrictMode>,
-)
+);
